b2032b96b1048a97d94cf8bd29293dcfdb125aab
= <=>
2021-07-01 09:30:32 +0700

    finish

--- budget.py
+++ budget.py
@@ -1,7 +1,100 @@
+BUFFER_SIZE = 30
+DES_MAX_LENGTH = 23
+AMO_MAX_LENGTH = 7
+
 class Category:
+    
+    ledger = list()
+    balance = int()
 
+    def __init__(self, name):
+        self.name = name
+        self.ledger = []
+        self.balance = 0
 
+    def check_funds(self, amount):
+        return amount <= self.balance
 
+    def deposit(self, amount, description = ''):
+        self.ledger.append({'amount' : amount, 'description' : description})
+        self.balance += amount
 
+    def withdraw(self, amount, description = ''):
+        if self.check_funds(amount):
+            self.ledger.append({'amount' : -amount, 'description' : description})
+            self.balance -= amount
+            return True
+        return False
+    
+    def get_balance(self):
+        return self.balance
 
+    def transfer(self, amount, destination):
+        check = self.withdraw(amount, 'Transfer to ' + destination.name)
+        if check:
+            destination.deposit(amount, 'Transfer from ' + self.name)
+        return check
+    
+    def __str__(self):
+        half = (BUFFER_SIZE - len(self.name)) // 2
+        buffer = '*' * half + self.name + '*' * (BUFFER_SIZE - half - len(self.name)) + '\n'
+        for item in self.ledger:
+            std_description = item['description'][:DES_MAX_LENGTH]
+            std_amount = "%.2f" % round(float(item['amount']), 2)
+            std_amount = std_amount[:min(AMO_MAX_LENGTH, BUFFER_SIZE - DES_MAX_LENGTH)]
+            buffer += std_description + ' ' * (BUFFER_SIZE - len(std_description) - len(std_amount)) + std_amount + '\n'
+        buffer += 'Total: ' + "%.2f" % round(float(self.balance), 2)
+        return buffer
+    
 def create_spend_chart(categories):
+    total_withdraw = 0
+    each_withdraw = {}
+    chart = "Percentage spent by category\n"
+
+    for category in categories:
+        withdraw = 0
+        for item in category.ledger:
+            if item['amount'] < 0:
+                total_withdraw += -item['amount']
+                withdraw += -item['amount']
+        each_withdraw[category] = withdraw
+
+    percentage = {}
+    for category in categories:
+        percentage[category] = int(round(each_withdraw[category] / total_withdraw * 100, 0))
+
+    for i in range(100, -10, -10):
+        temp = str(i)
+        while len(temp) < 3:
+            temp = ' ' + temp
+        chart += temp + '|'
+        for category in categories:
+            if percentage[category] >= i: 
+                chart += ' o '
+            else:
+                chart += '   '
+        chart += ' \n'
+    chart += ' ' * 4
+    for i in range(len(percentage)):
+        chart += '---'
+    chart += '-\n'
+    check = True
+    i = 0
+    while check:
+        chart += ' ' * 4
+        check = False
+        for category in categories:
+            if i < len(category.name):
+                check = True
+                if i == 0:
+                    chart += ' ' + category.name[i].capitalize() + ' '
+                else:
+                    chart += ' ' + category.name[i] + ' '
+            else:
+                chart += '   '
+        if check: chart += ' \n'
+        i += 1
+    temp = chart[0:-14]
+
+    return temp
+
--- main.py
+++ main.py
@@ -3,23 +3,39 @@ import budget
 from budget import create_spend_chart
 from unittest import main
 
-food = budget.Category("Food")
-food.deposit(1000, "initial deposit")
-food.withdraw(10.15, "groceries")
-food.withdraw(15.89, "restaurant and more food for dessert")
-print(food.get_balance())
-clothing = budget.Category("Clothing")
-food.transfer(50, clothing)
-clothing.withdraw(25.55)
-clothing.withdraw(100)
-auto = budget.Category("Auto")
-auto.deposit(1000, "initial deposit")
-auto.withdraw(15)
+# food = budget.Category("Food")
+# food.deposit(1000, "initial deposit")
+# food.withdraw(10.15, "groceries")
+# food.withdraw(15.89, "restaurant and more food for dessert")
+# print(food.get_balance())
+# clothing = budget.Category("Clothing")
+# food.transfer(50, clothing)
+# clothing.withdraw(25.55)
+# clothing.withdraw(100)
+# auto = budget.Category("Auto")
+# auto.deposit(1000, "initial deposit")
+# auto.withdraw(15)
 
-print(food)
-print(clothing)
-
-print(create_spend_chart([food, clothing, auto]))
+# print(food)
+# print(clothing)
+# print(auto)
+# print(create_spend_chart([food, clothing, auto]))
 
 # Run unit tests automatically
 main(module='test_module', exit=False)
+
+# food = budget.Category("Food")
+# entertainment = budget.Category("Entertainment")
+# business = budget.Category("Business")
+
+# food.deposit(900, "deposit")
+# entertainment.deposit(900, "deposit")
+# business.deposit(900, "deposit")
+# food.withdraw(105.55)
+# entertainment.withdraw(33.40)
+# business.withdraw(10.99)
+# print(create_spend_chart([business, food, entertainment]))
+
+
+# "Percentage spent by category\n100|          \n 90|          \n 80|          \n 70|    o     \n 60|    o     \n 50|    o     \n 40|    o     \n 30|    o     \n 20|    o  o  \n 10|    o  o  \n  0| o  o  o  \n    ----------\n     B  F  E  \n     u  o  n  \n     s  o  t  \n     i  d  e  \n     n     r  \n     e     t  \n     s     a  \n     s     i  \n           n  \n           m  \n           e  \n           n  \n           t  "
+# 'Percentage spent by category\n100|          \n 90|          \n 80|          \n 70|    o     \n 60|    o     \n 50|    o     \n 40|    o     \n 30|    o     \n 20|    o  o  \n 10|    o  o  \n  0| o  o  o  \n    ----------\n     B  F  E  \n     u  o  n  \n     s  o  t  \n     i  d  e  \n     n     r  \n     e     t  \n     s     a  \n     s     i  \n           n  \n           m  \n           e  \n           n  \n           t  '
